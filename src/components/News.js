a246142823e72d015240f83beef7a8600526ef34
Rahul <rahul02dahiya@gmail.com>
2023-10-22 01:12:13 +0530

    Navbar created : next step will be access api

---
+++ src/components/News.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import NewsItem from './NewsItem'
+
+export default class News extends Component {
+  render() {
+    return (
+      <div>News
+        <NewsItem/>
+        <NewsItem/>
+        <NewsItem/>
+        <NewsItem/>
+        <NewsItem/>
+      </div>
+    )
+  }
+}
